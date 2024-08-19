d059ee13660a02d1c5b672cc2916b3dcd025b7aa
soilhasnoblanket1 <166317388+soilhasnoblanket1@users.noreply.github.com>
2024-08-19 17:50:47 +0545

    Update route.js

--- events/route.js
+++ events/route.js
@@ -129,7 +129,7 @@ app.get("/feed", (req, res) => {
   });
 });
 
-app.get("/bin/cementglazeddoughnuts/adminpanel", (req, res) => {
+app.get("/bin/sijendaddy/adminpanel", (req, res) => {
   Confession.find()
    .then((confessions) => {
       res.render("admin", { 
